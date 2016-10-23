24a9cd868ab02a18aed577a6066b788a895da108
Willem Melis <r0348639@tienen.cs.kotnet.kuleuven.be>
2016-10-23 14:13:52 +0200

    added the new simulations to the sim data file

--- Session2/HW/generate_data.sh
+++ Session2/HW/generate_data.sh
@@ -3,28 +3,50 @@
 echo 'compiling the tools'
 # make
 
-echo 'generate the data'
-
-#part 1
-
 echo 'generating data part1'
-echo '' > ./data/part1/HW_part1_serial 
-for i in `seq 1 10`; do ./HW_part1.out $((2**$i)) 500 10  ; done | tee ./data/part1/HW_part1_serial
-echo '' > ./data/part1/HW_part1_parallel
-for i in `seq 1 10`; do ./HW_part1_par.out $((2**$i)) 500 10  ; done | tee ./data/part1/HW_part1_parallel
+i_max=14
+numberOfSim=500
+numberOfDiscarted=50
+dataLocation="./data/part1"
+
+rm $dataLocation/*
+for i in `seq 1 $i_max`; do ./HW_part1.out $((2**$i)) $numberOfSim $numberOfDiscarted  ; done | tee "$dataLocation/HW_part1_serial"
+for i in `seq 1 $i_max`; do ./HW_part1_sec_level0.out $((2**$i)) $numberOfSim $numberOfDiscarted ; done | tee "$dataLocation/HW_part1_serial_level0"
+for i in `seq 1 $i_max`; do ./HW_part1_sec_level3.out $((2**$i)) $numberOfSim $numberOfDiscarted ; done | tee "$dataLocation/HW_part1_serial_level3"
+
+for i in `seq 1 $i_max`; do ./HW_part1_par.out $((2**$i)) $numberOfSim $numberOfDiscarted ; done | tee "$dataLocation/HW_part1_parallel"
+for i in `seq 1 $i_max`; do OMP_NUM_THREADS=2 ./HW_part1_par.out $((2**$i)) $numberOfSim $numberOfDiscarted ; done | tee "$dataLocation/HW_part1_parallel_t2"
+for i in `seq 1 $i_max`; do OMP_NUM_THREADS=3 ./HW_part1_par.out $((2**$i)) $numberOfSim $numberOfDiscarted ; done | tee "$dataLocation/HW_part1_parallel_t3"
+
+for i in `seq 1 $i_max`; do ./HW_part1_par_l3.out $((2**$i)) $numberOfSim $numberOfDiscarted ; done | tee "$dataLocation/HW_part1_level3_parallel"
+for i in `seq 1 $i_max`; do OMP_NUM_THREADS=2 ./HW_part1_par_l3.out $((2**$i)) $numberOfSim $numberOfDiscarted ; done | tee "$dataLocation/HW_part1_parallel_level3_t2"
+for i in `seq 1 $i_max`; do OMP_NUM_THREADS=3 ./HW_part1_par_l3.out $((2**$i)) $numberOfSim $numberOfDiscarted ; done | tee "$dataLocation/HW_part1_parallel_level3_t3"
 
 echo 'generating data part2'
-mp=10
 numberOfSim=5000
-echo '' > ./data/part1/HW_part2_l0
-for i in `seq 1 $mp`; do ./HW_part2_l0.out $((2**$i)) $numberOfSim 10  ; done | tee ./data/part1/HW_part2_l0
-echo '' > ./data/part1/HW_part2_l3
-for i in `seq 1 $mp`; do ./HW_part2_l3.out $((2**$i)) $numberOfSim 10  ; done | tee ./data/part1/HW_part2_l3
-echo '' > ./data/part1/HW_part2_i_l0
-for i in `seq 1 $mp`; do ./HW_part2_l0_opti.out $((2**$i)) $numberOfSim 10  ; done | tee ./data/part1/HW_part2_i_l0
-echo '' > ./data/part1/HW_part2_i_l3
-for i in `seq 1 $mp`; do ./HW_part2_l3_opti.out $((2**$i)) $numberOfSim 10  ; done | tee ./data/part1/HW_part2_i_l3
+numberOfDiscarted=50
+dataLocation="./data/part2"
+sizeVector=1000
 
+rm $dataLocation/*
+echo 'in cpp level0:' | tee "$dataLocation/data_sim_part2" 
+./HW_part2_l0.out $sizeVector $numberOfSim numberOfDiscarted | tee -a  "$dataLocation/data_sim_part2"
+echo 'in cpp level3:' | tee -a "$dataLocation/data_sim_part2" 
+./HW_part2_l3.out $sizeVector $numberOfSim numberOfDiscarted | tee -a  "$dataLocation/data_sim_part2"
+echo 'in hpp level0:' | tee -a "$dataLocation/data_sim_part2" 
+./HW_part2_l0_opti.out $sizeVector $numberOfSim numberOfDiscarted | tee -a "$dataLocation/data_sim_part2"
+echo 'in hpp level3:' | tee -a  "$dataLocation/data_sim_part2" 
+./HW_part2_l3_opti.out $sizeVector $numberOfSim numberOfDiscarted | tee -a "$dataLocation/data_sim_part2"
 
+echo 'generating data part3'
+#i_max=8
+numberOfSim=500
+numberOfDiscarted=50
+dataLocation="./data/part3"
 
-
+rm $dataLocation/*
+for i in `seq 1 $i_max`; do ./HW_part3_par.out $((2**$i)) $numberOfSim numberOfDiscarted  ; done | tee "$dataLocation/HW_part3_par"
+for i in `seq 1 $i_max`; do ./HW_part3_par_l3.out $((2**$i)) $numberOfSim numberOfDiscarted  ; done | tee "$dataLocation/HW_part3_par"
+for i in `seq 1 $i_max`; do ./HW_part3_serial.out $((2**$i)) $numberOfSim numberOfDiscarted  ; done | tee "$dataLocation/HW_part3_serial"
+for i in `seq 1 $i_max`; do ./HW_part3_serial_level0.out $((2**$i)) $numberOfSim numberOfDiscarted  ; done | tee "$dataLocation/HW_part3_serial_level0"
+for i in `seq 1 $i_max`; do ./HW_part3_serial_level3.out $((2**$i)) $numberOfSim numberOfDiscarted  ; done | tee "$dataLocation/HW_part3_serial_level3"
