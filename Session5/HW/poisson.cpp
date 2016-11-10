4931f650fac103b0029223a5e47020f8f70da2c4
Zilleplus <zilleplus@gmail.com>
2016-11-10 23:00:10 +0100

    added working protoype of the 1D-poisson solver

--- Session5/HW/poisson.cpp
+++ Session5/HW/poisson.cpp
@@ -4,71 +4,104 @@
 #include <typeinfo>
 #include <type_traits>
 #include <math.h>
+#include<iomanip>
+#include"element_apply.hpp"
 
-template<typename T>
+/* functor class of matvec */
+template<typename T,typename T_h>
 class Matvec{
     private:
-        double h;
+        T_h h;
     public:
-    Matvec(double h){
+    Matvec(T_h h){
         this->h=h;
     }
-    /* the matrix A is allready compensating for the minus side from the f */
+    /* the matrix A is already compensating for the minus side from the f */
     void operator() ( T const& x, T& y) const{
         for(int i=1;i<x.size()-1;i++){
-            y[i] = (x[i-1] - 2*x[i] + x[i-1]) \
-                   /h;
+            y[i] = (x[i-1] - 2*x[i] + x[i+1]) \
+                   /pow(h,2);
         }
-        y[0] = -2*x[0] + x[1];
-        int lastElementIndex=y.size()-1; 
-        y[lastElementIndex] = x[lastElementIndex-1] -2*x[lastElementIndex];
+        /* 2 exceptions on the borders */
+        y[0] = (-2*x[0] + x[1])/pow(h,2);
+        int lastElementIndex=y.size()-1;
+        y[lastElementIndex] = (x[lastElementIndex-1] -2*x[lastElementIndex])/pow(h,2);
     }
 };
 template<typename T>
 auto max_norm(T& x,T& y){
     auto maxnorm = std::abs(x[0] - y[0]);
     for(int i=1;i<x.size();i++){
-        if(maxnorm < std::abs(x[i] - y[i])){
-            maxnorm = std::abs(x[i] - y[i]);  
-        } 
+        auto new_maxnorm = std::abs(x[i] - y[i]);
+        if(maxnorm < new_maxnorm){
+            maxnorm = new_maxnorm;
+        }
     }
     return maxnorm;
-
 }
+/* functor class of matvec */
+template<typename T>
+class Example_functor{
+private:
+    T sigma;
+public:
+    Example_functor(T sigma){
+        this->sigma=sigma;
+    }
+    /* the matrix A is already compensating for the minus side from the f */
+    T operator() ( T x) {
+        return exp((pow(x,2))/sigma);
+    }
+};
 
 int main(int argc,char** args) {
-    int n=100;
+    /*
+     * use the element_apply.hpp from the first part of the homework
+     */
+    tws::vector<long double> testVector(5) ;
+    for (int i=0; i<testVector.size(); ++i) {
+        testVector[i] = i+1;
+    }
+    Example_functor<double>  example_functor(2);
+    transform_with_function(example_functor,testVector);
+
+    /*
+     * Solve the 1D poisson equation, par 2,3,4 of the homework assignment
+     */
+    int n;
     /* if an argument is present it must be n */
     args++;
     if(argc > 1)n = atoi(*args);
-    
-    tws::vector<double> b(n) ;
-    tws::vector<double> sol(n) ;
-    tws::vector<double> x(n) ;
-    tws::vector<double> b_ex(n) ;
-    
-    double h = 1/(double(n)+1);
+
+    tws::vector<long double> b(n) ;
+    tws::vector<long double> s(n) ;
+    tws::vector<long double> x(n) ;
+
+    long double h = 1/(double(n)+1);
     for (int i=0; i<x.size(); ++i){
-        x[i] = i*h;
-        b[i] = (3*x[i]+pow(x[i],2))*exp(x[i]);
+        x[i] = (i+1)*h;
+        b[i] = (3*x[i]+pow(x[i],2.0))*exp(x[i]);
     }
-    
-    Matvec <tws::vector<double> > matvec(h);
-    
+
+    /* define the functor */
+    Matvec <tws::vector<long double>, long double > matvec(h);
     /* calculate the solution of the 1D poisson */
-    tws::cg( matvec, x, b, 1.e-10, n ) ;
-    
+    tws::cg( matvec, x, b, 1.e-10, n );
+
     /* find the exact solution */
     for(int i=0;i<x.size();i++){
-        b_ex[i]= (x[i]-pow(x[i],2))*exp(x[i]);
+        s[i]= (x[i]-pow(x[i],2))*exp(x[i]);
     }
-    
-    double max_error = max_norm<tws::vector<double>>(b_ex,x);
-    std::cout<<max_error<<"\n";
-    std::cout<<b_ex<<"\n";
-    std::cout<<b<<"\n";
-    //std::cout<<"relative error: "<<tws::norm_2(sol-b_ex)/tws::norm_2(b_ex)<<std::endl;
-    //std::cout<<"x"<<x<<std::endl;
-    //std::cout<<"sol"<<sol<<std::endl;
+
+    double max_norm_err = max_norm<tws::vector<long double>>(s,x);
+
+    std::cout
+            << std::setprecision(std::numeric_limits<long double>::digits10+1)
+            << std::scientific;
+    std::cout<<"n="<< n << " max_norm_error=" << max_norm_err<< "\t"<< std::endl;
+
+//    std::cout<<"x"<<x<<std::endl;
+//    std::cout<<"sol"<<sol<<std::endl;
     return 0 ;
-} 
+
+}
