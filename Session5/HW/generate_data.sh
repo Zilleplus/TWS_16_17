65696892394c887c2366d2761f6381c4b371c64c
zilleplus <zilleplus@gmail.com>
2016-11-11 14:42:59 +0100

    added script to generate data

---
+++ Session5/HW/generate_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm *.data
+i_max=16
+for i in `seq 1 $i_max`; do ./poissonFloat.out $((2**$i))   ; done | tee floats.data
+for i in `seq 1 $i_max`; do ./poissonDouble.out $((2**$i))   ; done | tee double.data
+for i in `seq 1 $i_max`; do ./poissonLongDouble.out $((2**$i))   ; done | tee longDouble.data
+
