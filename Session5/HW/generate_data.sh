5fcb13470becfebf88aee55677b854495c2fd82a
Willem Melis <r0348639@voeren.cs.kotnet.kuleuven.be>
2016-11-14 17:34:16 +0100

    final data added ready for submit

--- Session5/HW/generate_data.sh
+++ Session5/HW/generate_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm *.data
-i_max=16
+i_max=18
 for i in `seq 1 $i_max`; do ./poissonFloat.out $((2**$i))   ; done | tee floats.data
 for i in `seq 1 $i_max`; do ./poissonDouble.out $((2**$i))   ; done | tee double.data
 for i in `seq 1 $i_max`; do ./poissonLongDouble.out $((2**$i))   ; done | tee longDouble.data
