65f91ea67735759cf63bac276b87928ee18e7daa
Willem Melis <r0348639@tienen.cs.kotnet.kuleuven.be>
2016-11-17 22:28:23 +0100

    added draft version of single core version

---
+++ Session6/HW/part2/main.f95
@@ -0,0 +1,30 @@
+program main
+    implicit none
+
+    integer,parameter :: N=4
+    integer, parameter :: wp= selected_real_kind(4)
+	complex, parameter :: minus_one=(-1)
+	integer :: i,j
+    complex(kind=wp), dimension(N,N) :: A,D,H
+	real :: b,c,pi
+
+	! define pi
+	PI=DACOS(-1.D0)
+
+	! build A
+    do i= 1,N
+        do j = 1,N
+            call random_number(b)
+            call random_number(c)
+            A(i,j) = sqrt(-2*log(b))*exp(2*PI*sqrt(minus_one)*c)
+        end do
+    end do
+	D=A
+
+	! H=A*AT 
+    !        TRANSA, TRANSB, M, N,  K,  ALPHA, A, LDA, B, LDB, BETA, C, LDC
+    call cgemm('N' ,   'C' , N, N, 0_wp, 0_wp, A, N,   D, N,   0 ,   H, N)
+
+	! cholesky decomposition
+
+end program
