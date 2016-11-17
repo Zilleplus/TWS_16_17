d6dbb75426b3eae7c9ca93af654b57c25feac259
Willem Melis <r0348639@tienen.cs.kotnet.kuleuven.be>
2016-11-17 20:45:19 +0100

    added working version of multi precisioun

---
+++ Session6/HW/part1/multi_precision.f90
@@ -0,0 +1,64 @@
+! WIT Willem Melis
+! gecompiler commando:
+! gfortran  fmsave.o  FM.o  FMZM90.o mpfun90.o mpmod90.o mpmodm90.o mpmodx90.o multi_precision.f90 
+program main
+    ! import lib from assignment in class
+    USE FMZM
+    ! import of an other mulit-precision lib
+    use mpfunmod
+    use mpmodule
+    IMPLICIT NONE
+    integer i
+    TYPE (FM), SAVE :: x,x_new,a,diff
+    type (mp_real) x_funmod90,x_new_mpfun90,diff_mpfun90
+
+    ! ----------------------------------------------
+    ! package used in class
+    print *, "------ USING FMZM ------"
+
+    CALL FM_SET(55)
+
+    x=TO_FM('0.9878')
+    a=TO_FM('3')
+
+    print * , 'starting value:'
+    call FM_PRINT(x)
+
+    ! do a max of 10 iterations
+    do i=1,10
+        x_new = x + cos(x**14) / (sin(x**14)*14*x**13)
+        diff = abs(x-x_new)
+        if (diff < 1.0D-55) then
+            print *, i
+            exit
+        end if 
+        call FM_PRINT(x_new)
+        x=x_new
+    end do
+
+    ! ----------------------------------------------
+    ! new package
+    
+    print *, "------ USING mpfun90 ------"
+
+    CALL MPINIT() !init function !!
+    ! set the precision to 55 digits
+    CALL MPSETPREC(55) ! this is undocumented as far as i could tell, the subroutine can be found in mpmod90.f line 133 in my version
+
+    x_funmod90 = '0.9878'
+    
+    ! do a max of 10 iterations
+    do i=1,10
+        x_new_mpfun90 = x_funmod90 + cos(x_funmod90**14) / (sin(x_funmod90**14)*14*x_funmod90**13)
+        diff_mpfun90 = abs(x_funmod90-x_new_mpfun90)
+        if (diff_mpfun90 < 1.0D-55) then
+            print *, i
+            exit
+        end if
+        CALL MPWRITE(6,x_funmod90)
+        x_funmod90=x_new_mpfun90
+    end do
+    
+    CALL MPWRITE(6,x_funmod90)
+    
+end program
