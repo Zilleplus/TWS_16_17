61cbd94b0a4ebc7589a553d8e31e3de43f9146f7
zilleplus <zilleplus@gmail.com>
2016-11-18 17:31:41 +0100

    added compiler command

--- Session6/HW/part2/main.f95
+++ Session6/HW/part2/main.f95
@@ -1,17 +1,18 @@
+! gfortran main.f95 -llapack -lblas
 program main
     implicit none
 
     integer,parameter :: N=4
     integer, parameter :: wp= selected_real_kind(4)
-	complex, parameter :: minus_one=(-1)
-	integer :: i,j
+    complex, parameter :: minus_one=(-1)
+    integer :: i,j
     complex(kind=wp), dimension(N,N) :: A,D,H
 	real :: b,c,pi
 
-	! define pi
-	PI=DACOS(-1.D0)
+    ! define pi
+    PI=DACOS(-1.D0)
 
-	! build A
+    ! build A
     do i= 1,N
         do j = 1,N
             call random_number(b)
@@ -19,12 +20,21 @@ program main
             A(i,j) = sqrt(-2*log(b))*exp(2*PI*sqrt(minus_one)*c)
         end do
     end do
-	D=A
+    call printMatrix(A,N,N)
+    D=A
 
-	! H=A*AT 
+    ! H=A*AT 
     !        TRANSA, TRANSB, M, N,  K,  ALPHA, A, LDA, B, LDB, BETA, C, LDC
     call cgemm('N' ,   'C' , N, N, 0_wp, 0_wp, A, N,   D, N,   0 ,   H, N)
 
-	! cholesky decomposition
+    ! cholesky decomposition
+    contains
+        subroutine printMatrix(matrix,n,m)
+            complex(kind=wp), dimension(N,N) :: matrix
+            integer :: n,m,i
+            do i=1,n
+                print*, matrix(i,1:m)
+            end do
+        end subroutine
 
 end program
