cc3f87b89665357f0a29244082d282b905818180
Willem Melis <r0348639@tienen.cs.kotnet.kuleuven.be>
2016-12-28 00:03:10 +0100

    added report

--- Session8/HW/matvec.hpp
+++ Session8/HW/matvec.hpp
@@ -1,3 +1,42 @@
+/*
+ * Willem Melis (r0348639)
+ * WIT
+ * tijd: 12 uur (lang zoeken op 3 fouten)
+ *
+ * complexiteit van y
+ *
+ * De term beta*x is van orde N, en wordt verwaarloost.
+ * Een matric vector product is van orde N^2
+ * 
+ * ---------------------------------------------------------------
+ * Met temp:
+ * temp=matrix vector product=N^2
+ * X^T*temp = (matrix vector voor temp) + matrix vector dit blijft in de orde N^2
+ * ---------------------------------------------------------------
+ * Zonder temp:
+ * Zonder de temp variabele moet het vector product X*x N maal 
+ * berekend worden. Dit zorgt ervoor dat complexiteit met N omhoog gaat.
+ * X^T*(X*x) = N*(matrix vector) = N^3
+ * ---------------------------------------------------------------
+ *
+ * Op graph.pdf is duidelijk te zien dat de complexiteit van de 
+ * withTemp lager is dan de complexitiet van without temp.
+ *
+ * Als N gaat van 512 naar 1024:
+ *
+ * met temp: (verwachten N^3=8)
+ * 0.0067589/0.000741615 = 9.11 = 2^3.2
+ *
+ * zonder temp: (verwachten N^4=16)
+ * 4.1807/0.193188 = 21.64 = 2^4
+ *
+ * ---------------------------------------------------------------
+ *
+ * TODO: hoe de grafieken hermaken? testen uitvoeren?
+ *
+ */
+
+
 #ifndef MATVEC_HPP
 #define MATVEC_HPP
 #include "vector.hpp"
